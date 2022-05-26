e30b6572a7377bbfc3efbbaecc4c3ab85bfb914f
Sete777 <owleg250@hotmail.com>
2022-05-25 23:43:17 -0300

    colocado o componente AppContainer em arquivo

--- pages/login.js
+++ pages/login.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import {AppContainer} from '../src/components/AppContainerComponent';
 
-const AppContainer = props => <div>{props.children}</div>;
+const AppInput = props => (
+	<div>
+		<label>{props.label}</label>
+		<input type={props.type} title={props.title} />
+	</div>
+);
+const AppButton = props => (
+	<div>
+		<input type="submit" title={props.title} />
+	</div>
+);
 
 const Login = props => {
   return (
-    <AppContainer>
-      <div>
-        <label>Usuário: </label>
-        <input type="text" title="apelido ou email do usuário" />
-      </div>
-      <div>
-        <label>Senha: </label>
-        <input type="password" title="senha do usuário" />
-      </div>
-      <div>
-        <input type="submit" title="Entrar" />
-      </div>
-    </AppContainer>
-  );
+		<AppContainer>
+			<AppInput
+				title="apelido ou email do usuário"
+				label="Usuário:"
+				type="text"
+			/>
+			<AppInput 
+				label="Senha:" 
+				type="password" 
+				title="senha do usuário" 
+			/>
+			<AppButton title="Entrar" />
+		</AppContainer>
+	);
 };
 
 export default Login;
